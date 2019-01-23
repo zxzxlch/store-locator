cd5ddc09d7e6ed0d53ff13553a5ce2de35512c21
Daniel Khoo <danieljkhoo@gmail.com>
2019-01-23 15:32:14 +0800

    fixed map view

--- src/StoreLocator.tsx
+++ src/StoreLocator.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
-import GoogleMapReact from "google-map-react";
 import Marker from "app/Marker";
 import MapList from "./list/MapList";
 import MapSearchGroup from "./filter/MapSearchGroup";
 import Map from "./map/Map";
 
-import './styles.css';
+import "./styles.css";
 
 const AnyReactComponent: any = () => <div>hello</div>;
 
@@ -21,21 +20,9 @@ class StoreLocator extends React.Component<any, any> {
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: "80vh", width: "100%" }}>
+      <div>
         <MapSearchGroup />
         <Map />
-        {/* <Marker /> */}
-        {/* <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={"Kreyser Avrora"}
-          />
-        </GoogleMapReact> */}
       </div>
     );
   }
--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -1,38 +1,64 @@
-import * as React from 'react';
+import * as React from "react";
 import GoogleMapReact from "google-map-react";
-import MapList from 'app/list/MapList';
+import MapList from "app/list/MapList";
 const AnyReactComponent: any = () => <div>hello</div>;
-const Map: React.SFC<{}> = () => (
-  <div className='map'>
-    <div className='list'>
-      <MapList mapItems={
-        [
-          { title: 'Area 2', description: 'Blk 25 Ghim Moh Link #01-09 Singapore 270025', index: 1, distance: '10m' },
-          { title: 'Location 2', description: 'Blk 25 Ghim Moh Link #01-09 Singapore 270025', index: 2, distance: '10m' },
-          { title: 'Space 3', description: 'sdjkvnsdv sdjvlnsdvnsd', index: 3, distance: '10m' },
-          { title: 'Distance 4', description: 'dsadsadsd', index: 4, distance: '10m' },
-        ]
-      } />
-    </div>
-    <div className='view'>
 
-    
+class Map extends React.Component<any, any> {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
 
-    {/* <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={"Kreyser Avrora"}
+      <div className='map'>
+        <div className='list'>
+          <MapList
+            mapItems={[
+              {
+                title: "Area 2",
+                description: "Blk 25 Ghim Moh Link #01-09 Singapore 270025",
+                index: 1,
+                distance: "10m"
+              },
+              {
+                title: "Location 2",
+                description: "Blk 25 Ghim Moh Link #01-09 Singapore 270025",
+                index: 2,
+                distance: "10m"
+              },
+              {
+                title: "Space 3",
+                description: "sdjkvnsdv sdjvlnsdvnsd",
+                index: 3,
+                distance: "10m"
+              },
+              {
+                title: "Distance 4",
+                description: "dsadsadsd",
+                index: 4,
+                distance: "10m"
+              }
+            ]}
           />
-        </GoogleMapReact> */}
-    </div>
-  </div>
-);
+        </div>
+        <div className='view'>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo"
+            }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
-
-export default Map
+export default Map;
