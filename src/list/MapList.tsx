7fd99628a6283f67a340f983dad0092f06d8ee83
ryanashneil <ryanashneil@gmail.com>
2019-01-23 13:42:10 +0800

    Add font and list header

--- src/list/MapList.tsx
+++ src/list/MapList.tsx
@@ -7,6 +7,8 @@ interface IMapListProps {
 
 const MapList: React.SFC<IMapListProps> = (props) => (
   <div>
+    <span className='counter__text'>
+    Showing {props.mapItems.length} places near 1 fusionopolis view</span>
     {props.mapItems.map(item => <MapListItem {...item} />)}
   </div>
 );
